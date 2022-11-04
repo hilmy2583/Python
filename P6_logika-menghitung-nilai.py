47868626f167f86b3c7e8a1a7cda0cb30e2c572b
hilmy2583 <muhammadhilmy2583@gmail.com>
2022-11-04 13:13:13 +0700

    perbaharui menghitung nilai

--- P6_logika-menghitung-nilai.py
+++ P6_logika-menghitung-nilai.py
@@ -2,7 +2,7 @@ nama = input("Masukkan Nama Anda: ")
 kelas = input("Masukkan Kelas Anda: ")
 nilai = int(input("Masukkan Nilai Anda: "))
 
-if (nilai >= 90):
+if (nilai >= 90 and nilai <= 100):
   ket="Istimewa"
 elif (nilai >= 70):
   ket="Sangat Bagus"
